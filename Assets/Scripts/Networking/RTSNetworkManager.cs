8bac8ba00d8429018cb1b3e71c3422152db6109d
Hernan Quintana <hernan@jsatch.com>
2023-09-10 16:50:37 -0500

    Spawning unit click a spawner

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+    [SerializeField]
+    private GameObject unitSpawnerPrefab;
+    public override void OnServerAddPlayer(NetworkConnectionToClient networkConnection)
+    {
+        base.OnServerAddPlayer(networkConnection);
+
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawnerPrefab, 
+            networkConnection.identity.transform.position, 
+            networkConnection.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, networkConnection);
+    }
 }
